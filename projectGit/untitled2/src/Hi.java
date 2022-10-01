c48f392fcb4b32ec3a2e0c263ab768c0c3e99dd1
unknown <harut.har2006@gmail.com>
2022-10-01 15:09:02 +0400

    New file

---
+++ projectGit/untitled2/src/Hi.java
@@ -0,0 +1,25 @@
+public class Hi {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 9, 3, 3, -10, 24, 5, -52, 69, 0};
+        System.out.print("orig array = {");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("}");
+        copyArray(array);
+        System.out.print("copy array = {");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("}");
+    }
+    public static int[] copyArray(int[] array) {
+        int[] copyArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < copyArray.length; j++) {
+                copyArray[i] = array[i];
+            }
+        }
+        return copyArray;
+    }
+}
